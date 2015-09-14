1767fa2df79a439458e5672a529a2643e1db01aa
evdokimovm <tel89147925532@gmail.com>
2015-09-14 20:36:20 +1000

    add random for all slot vars

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ function runSlots() {
     var slotOne, slotTwo, slotThree;
     var images = ["http://i.imgur.com/9H17QFk.png", "http://i.imgur.com/9RmpXTy.png", "http://i.imgur.com/VJnmtt5.png"];
 
+    min = 1
+    max = 3
+    slotOne = Math.floor(Math.random() * (max - min + 1)) + min;
+    slotTwo = Math.floor(Math.random() * (max - min + 1)) + min;
+    slotThree = Math.floor(Math.random() * (max - min + 1)) + min;
+
     $(".logger").html("");
     $(".logger").html("Not A Win");
 
